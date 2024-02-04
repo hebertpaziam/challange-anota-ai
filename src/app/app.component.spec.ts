1ad199011a9080c712030fb0201bf97278e71dc9
Hebert Paziam <hebert.paziam>
2024-02-04 00:51:20 -0300

    feat: implementing SSR

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'challange-anota-ai' title`, () => {
+  it(`should have the 'challenge-anota-ai' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('challange-anota-ai');
+    expect(app.title).toEqual('challenge-anota-ai');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, challange-anota-ai');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, challenge-anota-ai');
   });
 });
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  BASE_API:
+    'http://aai-frontend-interview-mock-data.s3-website-sa-east-1.amazonaws.com/cardlist.json',
+};
